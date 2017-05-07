bad59d7dcddfb62524419cf4d541d30d6e6e5807
Sean Hornsby <sean.hornsby@gmail.com>
2017-05-07 09:12:08 -0700

    Bricks setup

--- js/app.js
+++ js/app.js
@@ -33,7 +33,28 @@ window.onload = function() {
     ball.drawCircle(0,0,10)
     game.physics.arcade.enable(ball);
     // bricks
-
+    bricks = game.add.group()
+    let rows = 5;
+    let numberOfBricks = 20
+    let brickWidth = game.width / numberOfBricks
+    let brickHeight = 30
+    let brickOffset = 100
+    let colors = [
+      "0xCC0000",
+      "0xFFAE19",
+      "0xEEEE19",
+      "0x0000CC",
+      "0x009900"
+    ]
+    for (let i = 0; i < rows; i++ ) {
+      for (let f = 0; f < numberOfBricks; f++ ) {
+        console.log(i,f, f * brickWidth)
+        let brick = game.add.graphics(f * brickWidth, brickOffset + brickHeight * i)
+        brick.beginFill(colors[i%colors.length], 1)
+        brick.drawRect(0,0,brickWidth, brickHeight)
+        bricks.addChild(brick)
+      }
+    }
     // controls
   }
 
