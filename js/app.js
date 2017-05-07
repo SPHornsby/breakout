b18373297b578d5b29a18c4e805e9d827af17763
Sean Hornsby <sean.hornsby@gmail.com>
2017-05-07 07:15:11 -0700

    Intial setup

--- js/app.js
+++ js/app.js
@@ -1,15 +1,33 @@
 
 window.onload = function() {
+  const width = 800;
+  const height = 600;
 
-  const game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser', { preload: preload, create: create, update: update });
+  const game = new Phaser.Game(width, height, Phaser.AUTO, 'phaser', { preload: preload, create: create, update: update });
 
+  // setup
+  let player;
+  let ball;
+  let bricks;
+  let cursors;
 
   function preload () {
+
   }
   
   function create () {
+    // background
+
+    // player
+
+    // ball
+
+    // bricks
+
+    // controls
   }
 
   function update () {
+
   }
 };
